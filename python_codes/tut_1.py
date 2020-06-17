e9938a1e22cfb32367a84be1537ce791fe3811ff
pibo <piyush.bhojgude14@vit.edu>
2020-06-17 04:26:07 +0000

    Adding tut 2

--- python_codes/tut_1.py
+++ python_codes/tut_1.py
@@ -2,4 +2,7 @@
 friend_age = 30
 # Camel Case for class naming
 # Constant in all CAPS
+# variable name cannot start with number.
+# variable can only contain [[a-zA-Z][0-9]_]+
 PI = 3.14159
+RAD_TO_DEG = 180/PI
---
+++ python_codes/tut_2_numbers.py
@@ -0,0 +1,10 @@
+age = 35 # integer
+PI = 3.14159 # floating
+
+maths_operation = 1+3*4/2-2 #BODMAS is used
+print(maths_operation)
+# whenever we do division (/) in python we get float value in return
+# to get only the quotient in integer format use //
+print(10/3) # returns floating value 
+int_div = 10//3 # removes everything in front of decimal point
+print(int_div)
